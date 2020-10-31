64904360f29824e2db2d4522f91627cafa46d910
jennmez <jennmez@gmail.com>
2020-10-31 16:13:16 -0500

    making and passing tests for score page

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import grumpy_love from '../SVG/gc_love.svg';
+
 export default function Score({ score }) {
   return (
     <div>
       <h3>You got {score} questions correct.</h3>
+      <img src={grumpy_love} alt="grumpy_love" title="grumpy_love" />
     </div>
   );
 }
--- src/components/__tests__/Score.test.js
+++ src/components/__tests__/Score.test.js
@@ -9,12 +9,29 @@ describe('Score', () => {
   });
 
   test(`renders the player's score`, () => {
-    render(<Score score={8} />);
+    let score = 8;
+    render(<Score score={score} />);
 
     screen.getByRole('heading');
 
     expect(screen.getByRole('heading')).toHaveTextContent(
       'You got 8 questions correct.'
     );
+  });
+
+  test('renders correct img for highest score', () => {
+    render(<Score score={10} />);
+
+    screen.getByAltText(/grumpy_love/i);
+
+    expect(screen.getByAltText(/grumpy_love/i)).toBeInTheDocument();
+  });
+
+  test('renders correct img for lowest score', () => {
+    render(<Score score={1} />);
+
+    screen.getByAltText(/grumpy_food/i);
+
+    expect(screen.getByAltText(/grumpy_food/i)).toBeInTheDocument();
   });
 });
